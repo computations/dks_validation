e14694638d74a31af41aaaa1d2d54b043df96bcc
computations <bwbettisworth@gmail.com>
2019-04-29 11:12:27 +0200

    added a script to compute a blanket policy

---
+++ avx2sr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pickle
+
+with open('results.pkl', 'rb') as pkl:
+    results = pickle.load(pkl)
+
+best_times = []
+avx2sr_times = []
+ratios = []
+
+for dataset, times in results.items():
+    best_time = float('inf')
+    avx2sr_time = 0.0
+    for t in times:
+        if t['time'] < best_time:
+            best_time = t['time']
+        if t['siterepeats'] == 'on' and t['simd'] == 'avx2':
+            avx2sr_time = t['time']
+    best_times.append(best_time)
+    avx2sr_times.append(avx2sr_time)
+    ratios.append(avx2sr_time/best_time)
+
+print(ratios)
